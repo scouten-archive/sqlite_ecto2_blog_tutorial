0393560a41dec7772ec327f778b0837081e22cb2
Eric Scouten <eric@scouten.com>
2017-04-09 13:53:15 -0700

    Add db_connection.

--- mix.exs
+++ mix.exs
@@ -27,6 +27,7 @@ defmodule Blog.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:sqlite_ecto2, "~> 2.0.0-dev.2"}]
+    [{:db_connection, "~> 1.1.0"},
+     {:sqlite_ecto2, "~> 2.0.0-dev.0"}]
   end
 end
